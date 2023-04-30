2620538a3117bd3e0fffbbfec26e22124ffb2ec1
Chaein <pca0046@gmail.com>
2023-05-01 00:40:13 +0900

    ES Client 테스트

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { Client } from '@elastic/elasticsearch';
 
 @Injectable()
 export class AppService {
   async getHello(): Promise<string> {
-    const {data} = await axios.get("http://localhost:9200/test/_search", {
+    const { data } = await axios.get("http://localhost:9200/test/_search", {
       data: {
         "query": {
           "match": {
@@ -14,6 +15,19 @@ export class AppService {
       }
     })
 
-    return data.hits.hits[0]._source.name as string;
+    const client = new Client({
+      node: 'http://localhost:9200',
+    })
+    
+    const { hits } = await client.search<{name: string}>({
+      index: "test",
+      query: {
+        "match": {
+          "name": "ho"
+        }
+      }
+    })
+
+    return `api: ${data.hits.hits[0]._source.name}, client: ${hits.hits[0]._source!.name}` as string;
   }
 }
